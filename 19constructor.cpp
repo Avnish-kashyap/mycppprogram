93b680f911e9e0ee59a1e34d2947e541d60bb39c
Avnish-kashyap <166170124+Avnish-kashyap@users.noreply.github.com>
2024-04-22 11:22:01 +0530

    Add files via upload

---
+++ 19constructor.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle{
+public:
+    int length, breadth;
+
+    Rectangle(){        //defau;t constructor - no args
+        length = 3;
+        breadth = 2;
+    }
+
+    Rectangle(int x, int y){        //paarmeterised constructor - args passed
+        length = x;
+        breadth = y;
+    }
+
+    Rectangle(Rectangle& r){        //copy constructor - initialize obj by an existing obj
+        length = r.length;
+        breadth = r.breadth;
+    }
+};
+
+int main(){
+    Rectangle r1;
+    cout<<r1.length<<" "<<r1.breadth<<endl;
+
+    Rectangle r2(5,4);
+    cout<<r2.length<<" "<<r2.breadth<<endl;
+
+    Rectangle r3 = r2;
+    cout<<r3.length<<" "<<r3.breadth<<endl;
+
+    return 0;
+}
